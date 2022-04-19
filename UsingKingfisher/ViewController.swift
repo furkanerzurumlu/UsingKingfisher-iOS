35c6682543a4387141ae705fb338dbbb2c3d592f
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-04-19 15:51:30 +0300

    Bulid project

---
+++ UsingKingfisher/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UsingKingfisher
+//
+//  Created by Furkan Erzurumlu on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func image1(_ sender: Any) {
+    }
+    @IBAction func image2(_ sender: Any) {
+    }
+    
+
+}
+
