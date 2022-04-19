4c1e2e01eb13709a4591fa8d5be4f9c432bfdcd2
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-04-20 02:45:38 +0300

    Get image

--- UsingKingfisher/ViewController.swift
+++ UsingKingfisher/ViewController.swift
@@ -13,13 +13,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showPicture(imageName: "kofte.png")
     }
     @IBAction func image1(_ sender: Any) {
+        showPicture(imageName: "fanta.png")
     }
     @IBAction func image2(_ sender: Any) {
+        showPicture(imageName: "ayran.png")
     }
     
-
+    func showPicture(imageName:String){
+        if let url = URL(string: "http://kasimadalan.pe.hu/yemekler/resimler/\(imageName)"){
+            DispatchQueue.main.async {
+                self.imageView.kf.setImage(with: url)
+            }
+        }
+    }
 }
+
 
